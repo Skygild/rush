f2fa262ca3ad643f970d868c898ce4d57c7ed303
Bvin-in <152376297+Maviin05@users.noreply.github.com>
2024-12-17 22:52:06 +0800

    Update sdk.ts
    Transferred Signin function inside RushSdk class

--- ecs/web/sdk/typescript/src/modules/sdk/sdk.ts
+++ ecs/web/sdk/typescript/src/modules/sdk/sdk.ts
@@ -4,17 +4,8 @@ import { Keypair, PublicKey } from "@solana/web3.js";
 import bs58 from "bs58";
 
 /**
- * Generates a new Solana Keypair and returns it.
- * @returns {Keypair} A new Solana Keypair.
+ * RushSDK class that provides SDK functionality for interacting with Solana.
  */
-export function createKeypair(): Keypair {
-  const keypair = Keypair.generate();
-  console.log("New Keypair Created:");
-  console.log("Public Key:", keypair.publicKey.toBase58());
-  console.warn("Secret Key generated.");
-  return keypair;
-}
-
 export class RushSDK {
   // Important variables
   private keypair: Keypair;
@@ -31,7 +22,7 @@ export class RushSDK {
       this.keypair = Keypair.fromSecretKey(secret_key); // Create a key pair using the provided secret key
     } else {
       console.log("No secret key provided. Generating a new keypair.");
-      this.keypair = createKeypair(); // Generate a new keypair if no secret key is provided
+      this.keypair = this.Signin(); // Generate a new keypair if no secret key is provided
     }
 
     const programIdKey = new PublicKey(program_id);
@@ -43,10 +34,27 @@ export class RushSDK {
       signer: this.keypair,
     });
   }
+
+  /**
+   * Generates a new Solana Keypair and returns it.
+   * @returns {Keypair} A new Solana Keypair.
+   */
+  private Signin(): Keypair {
+    const keypair = Keypair.generate();
+    console.log("New Keypair Created:");
+    console.log("Public Key:", keypair.publicKey.toBase58());
+    console.warn("Secret Key generated.",keypair.secretKey);
+    return keypair;
+  }
 }
 
 // Example usage Test
 if (require.main === module) {
-  const keypair = createKeypair();
-  console.log("Use this keypair in future transactions.");
+  const sdk = new RushSDK({
+    rpc_url: 'https://api.mainnet-beta.solana.com', 
+    program_id: 'test', 
+    blueprint_path: 'test', 
+  });
+  console.log("Public Key:", sdk.keypair.publicKey.toBase58());
+  console.log("Secret Key ", sdk.keypair.secretKey);
 }
